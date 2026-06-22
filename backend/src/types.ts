93ee0a8bdac9ddbbcbfbf181c1322f943b8ed5cb
Your Name <122686159+samia-oishi@users.noreply.github.com>
2026-06-22 18:59:37 +0600

    Update types.ts

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -20,7 +20,6 @@ export type AppUser = {
   location?: string;
   settings: {
     theme: 'light' | 'dark' | 'system';
-    emailNotifications: boolean;
   };
   createdAt: Date;
   updatedAt: Date;
